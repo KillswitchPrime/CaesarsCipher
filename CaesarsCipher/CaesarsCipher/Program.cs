08ef2019bc35eec439a6468e7f070dc8db6c89e4
KillswitchPrime <flip_d_@hotmail.com>
2022-04-22 21:58:41 +0200

    Added input logic and shifter logic

--- CaesarsCipher/CaesarsCipher/Program.cs
+++ CaesarsCipher/CaesarsCipher/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("This program will encrypt and decrypt text using caesars cipher.");
+
+string message;
+string cipher;
+int shift;
+
+Regex regex = new(@"([,.A-Åa-å])\\w+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+do
+{
+    Console.Write("Input message: ");
+
+    message = Console.ReadLine();
+
+} while (string.IsNullOrWhiteSpace(message) is true || regex.IsMatch(message) is false);
+
+do
+{
+    Console.Write("Input cipher: ");
+
+    cipher = Console.ReadLine();
+
+} while (string.IsNullOrWhiteSpace(cipher) is true || regex.IsMatch(cipher) is false);
+
+do
+{
+    shift = -1;
+    
+    Console.Write("Input cipher: ");
+
+    var succesfullyParsed = int.TryParse(Console.ReadLine(), out var number);
+
+    if (succesfullyParsed) 
+    { 
+        shift = number; 
+    }
+
+} while (shift < 0);
+
---
+++ CaesarsCipher/CaesarsCipher/Shifter.cs
@@ -0,0 +1,31 @@
+﻿namespace CaesarsCipher
+{
+    public static class Shifter
+    {
+        public static string EncryptionShift(int shift, string message)
+        {
+            var shiftedMessage = "";
+
+            foreach (var symbol in message)
+            {
+                if (AlphabetRecord.Alphabet.Contains(symbol) is false)
+                {
+                    continue;
+                }
+
+                var currentIndex = AlphabetRecord.Alphabet.IndexOf(symbol);
+                var shiftedIndex = currentIndex + shift;
+
+                if (shiftedIndex > AlphabetRecord.Alphabet.Count)
+                {
+                    shiftedIndex -= AlphabetRecord.Alphabet.Count - 1;
+                }
+
+                shiftedMessage += AlphabetRecord.Alphabet[shiftedIndex];
+
+            }
+
+            return shiftedMessage.ToString();
+        }
+    }
+}
