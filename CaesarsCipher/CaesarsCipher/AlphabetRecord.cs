9983ed367acc171f6cf049e48dd68e27e754e758
KillswitchPrime <flip_d_@hotmail.com>
2022-04-22 21:58:17 +0200

    Added alphabet

---
+++ CaesarsCipher/CaesarsCipher/AlphabetRecord.cs
@@ -0,0 +1,38 @@
+﻿namespace CaesarsCipher
+{
+    internal static class AlphabetRecord
+    {
+        public static readonly List<char> Alphabet  = new()
+        {
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z',
+            'æ',
+            'ø',
+            'å'
+        };
+    }
+}
