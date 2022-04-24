c11063053b4c84f965d20161474cd46e0926253d
KillswitchPrime <flip_d_@hotmail.com>
2022-04-24 12:21:33 +0200

    Added decrypter and encrypter

--- CaesarsCipher/CaesarsCipher/Encrypter.cs
+++ CaesarsCipher/CaesarsCipher/Encrypter.cs
@@ -5,8 +5,63 @@
         public static string Encrypt(string cipher, string message)
         {
 
+            var cipherAlphabet = CreateCipherAlphabet(cipher);
 
-            return "";
+            var codedMessage = "";
+            foreach(var symbol in message.ToLower())
+            {
+                if(AlphabetRecord.Alphabet.Contains(symbol) is false)
+                {
+                    codedMessage += symbol;
+                    continue;
+                }
+
+                var indexOfSymbol = AlphabetRecord.Alphabet.IndexOf(symbol);
+
+                codedMessage += cipherAlphabet[indexOfSymbol];
+            }
+
+            return codedMessage;
+        }
+
+        public static string Decrypt(string cipher, string message)
+        {
+
+            var cipherAlphabet = CreateCipherAlphabet(cipher);
+
+            var codedMessage = "";
+            foreach (var symbol in message.ToLower())
+            {
+                if (AlphabetRecord.Alphabet.Contains(symbol) is false)
+                {
+                    codedMessage += symbol;
+                    continue;
+                }
+
+                var indexOfSymbol = cipherAlphabet.IndexOf(symbol);
+
+                codedMessage += AlphabetRecord.Alphabet[indexOfSymbol];
+            }
+
+            return codedMessage;
+        }
+
+        private static List<char> CreateCipherAlphabet(string cipher)
+        {
+            var cipherAlphabet = new List<char>();
+            foreach (var c in cipher)
+            {
+                cipherAlphabet.Add(c);
+            }
+
+            foreach (var symbol in AlphabetRecord.Alphabet)
+            {
+                if (cipherAlphabet.Contains(symbol)) continue;
+
+                cipherAlphabet.Add(symbol);
+            }
+
+            return cipherAlphabet;
         }
     }
 }
