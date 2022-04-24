e1c7ad26a64925ddcdfb5a063d1361d9373b4880
KillswitchPrime <flip_d_@hotmail.com>
2022-04-24 12:04:32 +0200

    Added decryption shift

--- CaesarsCipher/CaesarsCipher/Shifter.cs
+++ CaesarsCipher/CaesarsCipher/Shifter.cs
@@ -27,5 +27,31 @@
 
             return shiftedMessage.ToString();
         }
+
+        public static string DecryptionShift(int shift, string message)
+        {
+            var shiftedMessage = "";
+
+            foreach (var symbol in message.ToLower())
+            {
+                if (AlphabetRecord.Alphabet.Contains(symbol) is false)
+                {
+                    shiftedMessage += symbol;
+                    continue;
+                }
+
+                var currentIndex = AlphabetRecord.Alphabet.IndexOf(symbol);
+                var shiftedIndex = currentIndex - shift;
+
+                if (shiftedIndex < AlphabetRecord.Alphabet.Count)
+                {
+                    shiftedIndex += AlphabetRecord.Alphabet.Count - 1;
+                }
+
+                shiftedMessage += AlphabetRecord.Alphabet[shiftedIndex];
+            }
+
+            return shiftedMessage.ToString();
+        }
     }
 }
